401ed4e4dc60c0f68b957ebdb8203359f34bf2a9
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2023-01-05 20:11:56 -0300

    adicionando primeiro projeto em react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [finalResult, setFinalResult] = useState(0)
+
+  const firstConvertedNumber = parseInt(firstNumber)
+  const secondConvertedNumber = parseInt(secondNumber)
+
+  const valueFirstNumber = (evt) => {
+    setFirstNumber(evt.target.value)
+  }
+
+  const valueSecondNumber = (evt) => {
+    setSecondNumber(evt.target.value)
+  }
+
+  const sum = () => {
+    setFinalResult(firstConvertedNumber + secondConvertedNumber)
+  }
+
+  const subtraction = () => {
+    setFinalResult(firstConvertedNumber - secondConvertedNumber)
+  }
+
+  const multiplication = () => {
+    setFinalResult(firstConvertedNumber * secondConvertedNumber)
+  }
+
+  const division = () => {
+    setFinalResult(firstConvertedNumber / secondConvertedNumber)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label for="firstNumber">Insira o primeiro número:</label>
+        <input type="text" id="firstNumber" onChange={valueFirstNumber}></input>
+        <label for="secondNumber">Insira o segundo número:</label>
+        <input type="text" id="secondNumber" onChange={valueSecondNumber}></input>
+        <button onClick={sum}>Somar (+)</button>
+        <button onClick={subtraction}>Subtrair (-)</button> 
+        <button onClick={multiplication}>Multiplicar (X)</button>
+        <button onClick={division}>Dividir (:)</button>
+        <p>
+          O resultado da operação é: {finalResult}
+        </p>
       </header>
     </div>
   );
